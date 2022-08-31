077a1b85f5b01f211a772090bad779bec68c210d
Neel Patel <55906633+Patel177@users.noreply.github.com>
2022-08-31 09:14:25 +0530

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,12 @@
+configuration webserver
+{
+    node "localhost"
+    {
+        WindowsFeature IIS
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+    }      
+}
+      
